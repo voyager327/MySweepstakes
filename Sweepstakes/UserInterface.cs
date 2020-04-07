1f5aacb649a0214d100830dadbc5e4152d5a4052
alfred macasarte <voyager327@yahoo.com>
2020-04-07 09:08:04 -0500

    resubmit

---
+++ Sweepstakes/UserInterface.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sweepstakes
+{
+    public static class UserInterface
+    {
+         
+        public static int ContestantNumber = 0; 
+        public static string FirstName()
+        {
+            Console.WriteLine("Enter your first name.");
+            return Console.ReadLine();
+        }
+           
+        public static string LastName()
+        {
+            Console.WriteLine("Enter your last name.");
+            return Console.ReadLine();
+        }
+
+        public static string EmailAdd()
+        {
+            Console.WriteLine("Enter your Email Add.");
+            return Console.ReadLine();
+        }
+        public static int RegistrationNumber()
+        {
+            ContestantNumber = +1;
+            Console.WriteLine("Your registration number is " + ContestantNumber);
+            return ContestantNumber;
+        }
+
+
+       
+            
+        
+    }
+
+}
+    
+              
+    
+
